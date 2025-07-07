9d6d0cced97489db4d238f39726a3f33ec811084
martin1611 <69118181+martin1611@users.noreply.github.com>
2025-07-07 10:45:23 -0500

    .

--- FirmaXadesFrisby/obj/Debug/net8.0/FirmaXadesFrisby.AssemblyInfo.cs
+++ FirmaXadesFrisby/obj/Debug/net8.0/FirmaXadesFrisby.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirmaXadesFrisby")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1c79b4b27102355764df2e22bcfd267cd6d248a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirmaXadesFrisby")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirmaXadesFrisby")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
